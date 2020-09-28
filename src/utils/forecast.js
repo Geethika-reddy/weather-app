c670dcdf00750eaa0bb847d820fd81c13e3f0125
Siva Reddy <geethikareddyvari@gmail.com>
2020-09-28 14:56:09 +0530

    Init commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,16 @@
 const request = require("request")
 
 
-const forecast = (latitude, longitude,callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=ddc7fa90bef53936440edc57c9c36c3d&query= ' + latitude + ',' + longitude
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=ddc7fa90bef53936440edc57c9c36c3d&query= ' + latitude + ',' + longitude + '&unit=f'
 
     request({ url , json: true}, (error, { body }) =>{
         if (error){
-            callback('unable to connect service', undefined)
+            callback('unable to connect to weather service', undefined)
         } else if (body.error) {
             callback('unable to connect', undefined)
         } else {
-            callback(undefined, 'it is currently ' + body.current.temperature + ' degree out. ')
+            callback(undefined, 'Temperature: ' + body.current.temperature + ' Humidity: ' + body.current.humidity )
             
         }
     })
